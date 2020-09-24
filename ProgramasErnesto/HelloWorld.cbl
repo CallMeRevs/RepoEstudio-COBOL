d9a340b02fa0d48a4c1b124e2d81c165474f7c64
Ernesto Revs <rrreeevvvsss@gmail.com>
2020-09-24 12:16:54 -0500

    Programas de Ejemplos

---
+++ ProgramasErnesto/HelloWorld.cbl
@@ -0,0 +1,19 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello, World!"
+            STOP RUN.
+
+       END PROGRAM YOUR-PROGRAM-NAME.
